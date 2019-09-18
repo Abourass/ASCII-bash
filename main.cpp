dcf9afa1aae89be8a5c7ad9e8a762ffffaa625af
Abourass <abourassa@assetval.com>
2019-09-18 10:32:23 -0600

    Make it so number one

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// Create my initials in block letters on the screen
+#include <iostream>
+
+int main() {
+    std::cout << "    A    ";
+    std::cout << "	\n";
+    std::cout << "  A   A  ";
+    std::cout << "	";
+    std::cout << "BBBBBBBB \n";
+    std::cout << " A     A ";
+    std::cout << "	";
+    std::cout << "B       B\n";
+    std::cout << "A       A";
+    std::cout << "	";
+    std::cout << "B       B\n";
+    std::cout << "AAAAAAAAA";
+    std::cout << "	";
+    std::cout << "BBBBBBBB \n";
+    std::cout << "A       A";
+    std::cout << "	";
+    std::cout << "B       B\n";
+    std::cout << "A       A";
+    std::cout << "	";
+    std::cout << "B       B\n";
+    std::cout << "A       A";
+    std::cout << "	";
+    std::cout << "BBBBBBBB \n";
+    std::cout << "I'm inside your terminal now\n";
+    return 0;
+}
